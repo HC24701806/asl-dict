ad5e178bc0c320baf1777aa8ecf9a58c6c81c46a
Haolin Cong <haolin.cong@gmail.com>
2024-12-17 10:14:35 -0800

    update 12-17

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -1,7 +1,5 @@
-import numpy as np
 import cv2
 import mediapipe as mp
-import time
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
