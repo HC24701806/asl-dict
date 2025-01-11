9bab093278613ea24714374ee3c4b3af2ddff54f
Haolin Cong <haolin.cong@gmail.com>
2025-01-10 17:05:47 -0800

    update 1-10

--- mini_demo/run_model.py
+++ mini_demo/run_model.py
@@ -7,13 +7,6 @@ from torchvision.transforms import v2
 from concurrent.futures import ThreadPoolExecutor
 from model import Model
 
-# model
-model = Model()
-saved = torch.load('./models/v4/best.pth', map_location=torch.device('mps'))
-model.load_state_dict(saved['model'])
-model = model.to('mps')
-model = model.eval()
-
 # label to class
 classes = []
 with open('sample_classes.txt') as labels_file:
@@ -21,7 +14,16 @@ with open('sample_classes.txt') as labels_file:
     for line in content:
         classes.append(line[:-1])
 
-def predict(frames):
+# load model
+def load_model(path):
+    model = Model()
+    saved = torch.load(path, map_location=torch.device('mps'))
+    model.load_state_dict(saved['model'])
+    model = model.to('mps')
+    model = model.eval()
+    return model
+
+def predict(frames, model):
     # process data
     process = v2.Compose([
         v2.ToImage(),
@@ -40,6 +42,7 @@ def predict(frames):
     data = data.permute(0, 2, 1, 3, 4)
 
     # predict
+    
     data = data.to('mps')
     outputs = model(data)
     post_act = torch.nn.Softmax(dim=1)
@@ -47,8 +50,10 @@ def predict(frames):
     return pred
 
 # video input
+model = load_model('./models/v4/best.pth')
 video = cv2.VideoCapture(0)
 frames = []
+recording = False
 pred_classes = None
 pred_probs = None
 while video.isOpened():
@@ -56,17 +61,21 @@ while video.isOpened():
     if not ret:
         break
 
-    frames.append(frame)
-    if pred_classes != None:
+    if recording:
+        frames.append(frame)
+        cv2.circle(frame, (50, 50), 20, (0, 0, 255), -1)
+    elif pred_classes != None:
         for i in range(3):
             cv2.putText(frame, f'{classes[pred_classes[i]]}: {pred_probs[i]}', (50, 50 + 50 * i), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
     cv2.imshow('video', frame)
 
     k = cv2.waitKey(1)
     if k == ord(' '):
-        pred = predict(frames)
-        pred_classes = pred.indices[0].to('cpu')
-        pred_probs = pred.values[0].detach().to('cpu').numpy()
-        frames = []
+        if recording:
+            pred = predict(frames, model)
+            pred_classes = pred.indices[0].to('cpu')
+            pred_probs = pred.values[0].detach().to('cpu').numpy()
+            frames = []
+        recording = not recording
     elif k == ord('q'):
         break
